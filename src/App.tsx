3950ca4cdb5387a40d05c3370a4e679e5c34d43c
Abhishek Kumar <abhishekkumar8222@gmail.com>
2024-01-03 13:39:37 +0000

    Move to Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.svg'
 import './App.css';
 
-function App() {
+export const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +21,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
